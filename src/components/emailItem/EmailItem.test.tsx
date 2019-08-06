85f3ea49388e2a461b80c5d749056252b1a920a7
greatermans <greatermans29@gmail.com>
2019-08-07 02:19:41 +0700

    Add jest tes for EmailItem.tsx

--- src/components/emailItem/EmailItem.test.tsx
+++ src/components/emailItem/EmailItem.test.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import { shallow, configure  } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+
 import EmailItem from './EmailItem';
 
 describe('Email Item', () => {
-  it('should render component correctly', () => {
-    expect(1 == 1);
-  });
+    const props = {
+        from: {
+            name: 'Tester',
+            email: 'Tester@gmail.com',
+        }, 
+        subject: 'This is test subject',
+        body: 'This is email body',
+        time: '10:00',
+        isSelected: false,
+        index: 0,
+        onItemClick: () => {},
+        color: 'blue',
+    };
+
+    it('should render component correctly for normal state', () => {
+        const wrapper = shallow(<EmailItem {...props}/>);
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should render component correctly for selected state', () => {
+        const newProps = { ...props, isSelected: true }
+        const wrapper = shallow(<EmailItem {...newProps}/>);
+
+        expect(wrapper).toMatchSnapshot();
+    });
 });
