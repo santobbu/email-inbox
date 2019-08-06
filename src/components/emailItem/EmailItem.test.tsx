7d20bc30f86a2d0140d1624d8e7e59ec4ee153f0
greatermans <greatermans29@gmail.com>
2019-08-07 00:24:44 +0700

    Update readme

---
+++ src/components/emailItem/EmailItem.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import EmailItem from './EmailItem';
+
+describe('Email Item', () => {
+  it('should render component correctly', () => {
+    expect(1 == 1);
+  });
+});
